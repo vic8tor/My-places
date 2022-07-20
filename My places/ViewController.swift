5417b7bbaa06c45f6852e1e8c4b4edffc78bbfef
vic8tor <timongrinni@gmail.com>
2022-07-21 00:12:26 +0300

    Initial Commit

---
+++ My places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My places
+//
+//  Created by Victor on 21.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
