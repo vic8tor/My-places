55ee006b9a66d6aa913cfce972db05b9bfa4a324
vic8tor <timongrinni@gmail.com>
2022-07-22 00:40:33 +0300

    Added customTableViewCell

---
+++ My places/ViewControllers/PlacesTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesTableViewCell.swift
+//  My places
+//
+//  Created by Victor on 21.07.2022.
+//
+
+import UIKit
+
+class PlacesTableViewCell: UITableViewCell {
+    
+    @IBOutlet var placeImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    
+}
